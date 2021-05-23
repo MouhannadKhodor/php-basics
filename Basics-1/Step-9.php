9b6be47fbdd792da817578b59e8c7edffb485c6f
MouhannadAlKhodor <moesidaya@gmail.com>
2021-05-24 00:20:41 +0300

    Step-9

---
+++ Basics-1/Step-9.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    "Hello world, Help me I am using PHP"
+    <br/>
+    <br/>
+    <?php
+        $name = "Moe";
+        var_dump($name);
+        $variable = 'hey eyyo';
+        var_dump($variable);
+    ?>
+</body>
+
+</html>
+

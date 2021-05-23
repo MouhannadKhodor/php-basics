7db2f045218fa527b3c15e333ff5d20957275115
MouhannadAlKhodor <moesidaya@gmail.com>
2021-05-24 00:18:00 +0300

    Step-8-b

--- Basics-1/Step-8.php
+++ Basics-1/Step-8.php
@@ -15,6 +15,19 @@
     <?php
         $fruits = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues","Watermelon");
         echo 'Hello Sir, do you have '.$fruits[0].','.$fruits[1].', and '.$fruits[2].'? Also if you sell fruits can I find a '.$fruits[5].'?';
+
+        echo '<br/>';
+        echo '<br/>';
+        echo '<br/>';
+
+        $grocery = array (
+            array('eggs' , 'balade', 'mazere3' ),
+            array('milk','Fresh', 'Taanayel'),
+            array('water-pack', 'Tanoureen', 'Reem')
+        );
+
+        echo "Hey Sir, Please I need 1 pack of ".$grocery[0][1].' '.$grocery[0][0]." and 3 ".$grocery[2][2].' '. $grocery[2][0];
+
     ?>
 </body>
 

8b52ef840142642c60dc408441a3fcbe31121327
MouhannadAlKhodor <moesidaya@gmail.com>
2021-05-24 00:41:10 +0300

    B2 -- Step-4

---
+++ Basics-2/Step-4.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+       $num=407;  
+       $total=0;  
+       $x=$num;  
+       while($x!=0)  
+       {  
+       $rem=$x%10;  
+       $total=$total+$rem**3;  
+       $x=$x/10;  
+       }  
+       if($num==$total)  
+       {  
+       echo "Yes it is an Armstrong number";  
+       }  
+       else  
+       {  
+       echo "No it is not an armstrong number";  
+       }  
+    ?>
+</body>
+
+</html>

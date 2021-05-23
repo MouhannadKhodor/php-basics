a1304f182b6afe8e785726544f2577c781f5dbc9
MouhannadAlKhodor <moesidaya@gmail.com>
2021-05-23 23:26:19 +0300

    step-3

---
+++ Basics-1/Step-3.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    "Hello world, Help me I am using PHP"
+    <br/>
+    <br/>
+    <?php
+        echo "Hello world i'm in trouble";
+        echo '<br/>';
+        print "I am getting errors but I can't see the errors";
+        echo '<br/>';
+        $name = 'Moe';
+        $email = 'moesidaya@gmail.com';
+        print "My name is $name  and my email is $email";
+        $height = '2';
+        $width = '4';
+        $area = $height * $width;
+        echo '<br/>';
+        echo $area;
+
+    ?>
+</body>
+
+</html>
+
---
+++ Basics-1/Step-4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    "Hello world, Help me I am using PHP"
+    <br/>
+    <br/>
+    <?php
+        echo "Hello world i'm in trouble",
+        print "I am getting errors but I can't see the errors"
+    ?>
+</body>
+
+</html>
+

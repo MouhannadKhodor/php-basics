097559d41fd97c1577c47a9a3e3ff31651101402
MouhannadAlKhodor <moesidaya@gmail.com>
2021-05-24 00:05:34 +0300

    Step-8-a

---
+++ Basics-1/Step-8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    "Hello world, Help me I am using PHP"
+    <br/>
+    <br/>
+    <?php
+        $fruits = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues","Watermelon");
+        echo 'Hello Sir, do you have '.$fruits[0].','.$fruits[1].', and '.$fruits[2].'? Also if you sell fruits can I find a '.$fruits[5].'?';
+    ?>
+</body>
+
+</html>
+

9a8b6ec9ea30fad1b01ba30ac1222a7b46031363
MouhannadAlKhodor <moesidaya@gmail.com>
2021-05-24 11:12:22 +0300

    B3 -- for-ex-1

---
+++ Basics-3/For-Ex-1.php
@@ -0,0 +1,25 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    function sum_of_digits($nums)
+    {
+        $digits_sum = 0;
+        for ($i = 0; $i < strlen($nums); $i++) {
+            $digits_sum += $nums[$i];
+        }
+        return $digits_sum;
+    }
+    echo sum_of_digits("12345") . "\n";
+    echo sum_of_digits("9999") . "\n";
+    ?>
+</body>
+
+</html>
